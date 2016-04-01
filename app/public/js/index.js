86d354d329a3ba09b103ec6b6f866f5a86fc7f8d
Daniel F. Padilla <danielfpadilla@gmail.com>
2016-04-01 13:46:11 +0800

    Add retry and logs

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -11,30 +11,58 @@ angular.module('app', [])
              function($scope,
                       $http,
                       $timeout){
-    $scope.urls = ['Start'];
-    $scope.paths = {};
-    $scope.error = {};
+    function start (urls, size) {
+        var notFound = {},
+            notFoundSize = 0,
+            done = 0,
+            tryAgain = function () {
+                start(notFound, notFoundSize);
+            };
 
-    if (window.siteUrls) {
-        $scope.urls = siteUrls;
-    }
-
-    $scope.submit = function () {
-        for (var i in $scope.urls) {
+        for (var i in urls) {
             (function (i) {
                 $timeout(function() {
                     $http.post('/screenshot', {
-                            url: $scope.urls[i],
+                            url: urls[i],
                             filename: i
                         })
                         .then(function (result) {
                             $scope.paths[i] = result.data;
+
+                            done++;
+
+                            $scope.done++;
+
+                            if (done == size) {
+                                tryAgain();
+                            }
                         })
                         .catch(function (error) {
+                            done++;
+
+                            $scope.done++;
+
+                            notFound[i] = urls[i];
+                            notFoundSize++;
+
                             $scope.error[i] = error.status + ':' + error.statusText;
+
+                            if (done == size) {
+                                tryAgain();
+                            }
                         });
                 }, 1000);
             })(i);
         }
+    }
+
+    $scope.urls = window.siteUrls ? siteUrls: ['www.google.com'];
+    $scope.paths = {};
+    $scope.error = {};
+    $scope.total = $scope.urls.length;
+    $scope.done = 0;
+
+    $scope.submit = function () {
+        start($scope.urls, $scope.urls.length);
     };
 }]);
